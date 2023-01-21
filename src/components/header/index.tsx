2bc048b1c8ce419289015b88034ee50f7f90620b
ryota-09 <n.ryo950911@gmail.com>
2023-01-21 16:01:00 +0900

    ヘッダーの実装

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,19 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
 import { FC } from "react";
 
 const Header: FC = () => {
   return (
     <header className="header">
-      <div className="header-container">
+      {/* NOTE: コンテナサイズはヘッダーに合わせる(112px(100%)) */}
+      <div className="header-container w-container">
         <div className="site">
           <a href="">
             <img src="/next.svg" alt="" width="135" height="26" />
           </a>
         </div>
+        <button className="navbtn">
+          <i className="fas fa-bars"></i>
+          <span className="sr-only">SPボタン</span>
+        </button>
       </div>
-      <button className="navbtn">
-        <i className="fas fa-bars"></i>
-        <span className="sr-only">SPボタン</span>
-      </button>
     </header>
   );
 };
