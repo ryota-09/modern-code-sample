a7a423ec780e39fb8c24b3b29a147466dbf1b703
ryota-09 <n.ryo950911@gmail.com>
2023-01-28 20:39:15 +0900

    ヒーローの作成

---
+++ src/components/hero/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+const Hero: FC = () => {
+  return (
+    <section className="hero">
+      <div className="hero-container w-container">
+        <h1>h1テキスト</h1>
+        <p>pテキストpテキストpテキスト</p>
+        <a href="#">無料ではじめる</a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/header";
+import Hero from "@/components/hero";
 import { NextPage } from "next";
 
 export const Top: NextPage = () => {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 };
