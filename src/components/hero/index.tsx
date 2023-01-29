f25f401a2efe7eaf2a43e54a08d644b530b6dea3
ryota-09 <n.ryo950911@gmail.com>
2023-01-29 10:41:26 +0900

    ヒーローメモ作成

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,12 +1,22 @@
 import { FC } from "react";
 
+type Props<T> = {
+  data: T
+}
+
+const MyComponent = <T extends string>({data}: Props<T>) => {
+  return <div>{data}</div>
+}
+
+<MyComponent<string> data="Data" /> 
+
 const Hero: FC = () => {
   return (
     <section className="hero">
       <div className="hero-container w-container">
         <h1>h1テキスト</h1>
         <p>pテキストpテキストpテキスト</p>
-        <a href="#">無料ではじめる</a>
+        <a href="#" className="btn">無料ではじめる</a>
       </div>
     </section>
   );
